7eaf77cc1853e3720aa8937603f014f895a1e480
TatsiaA <taniokk@mail.ru>
2022-01-16 20:09:30 +0300

    feat: add array task solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -350,8 +350,14 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const arr1 = {
+    0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
+  };
+  const arr2 = {
+    zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9,
+  };
+  return arr.map((x) => arr2[x]).sort().map((x) => arr1[x]);
 }
 
 /**
